12b5acc53a2338d6b2954b61f599dd56f5089fac
ahmed-elnashar <a7medelnashar@hotmail.com>
2020-05-21 20:13:21 +0200

    end tut

--- app.js
+++ app.js
@@ -1,3 +1,61 @@
-var logger = require('./logger');
+// const logger = require('./logger');
 
-logger.log('message');
+// logger('message');
+
+//---------------------------------------
+
+// const path = require('path');
+
+// let pathObj = path.parse(__filename);
+
+// console.log(pathObj);
+
+//---------------------------------------
+
+// const os = require('os');
+
+// let totalMemory = os.totalmem();
+// let freeMemory = os.freemem();
+
+// console.log(`Total Memory: ${totalMemory} and the Free Memory: ${freeMemory}`);
+
+//---------------------------------------
+
+// const fs = require('fs');
+
+// // const files = fs.readdirSync('./');
+// // console.log(files);
+
+// fs.readdir('./', function (err, files) {
+//   if (err) console.log(Error, err);
+//   else console.log('Result', files);
+// });
+
+//---------------------------------------
+
+// const Logger = require('./logger');
+// const logger = new Logger();
+
+// // Register a listener
+// logger.on('messageLogged', eventArg => console.log('Listener called', eventArg));
+
+// logger.log('message');
+
+//---------------------------------------
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello world');
+    res.end();
+  }
+  if (req.url === '/api/courses') {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
--- logger.js
+++ logger.js
@@ -1,8 +1,16 @@
+const EventEmitter = require('events');
+
 var url = 'http://mylogger.io/log';
 
-function log(message) {
-  // Send an HTTP request
-  console.log(message);
+class Logger extends EventEmitter {
+  log(message) {
+    // Send an HTTP request
+    console.log(message);
+
+    // Raise an Event
+    // emitter.emit('messageLogged', { id: 1, url: 'http://' });
+    this.emit('messageLogged', { id: 1, url });
+  }
 }
 
-module.exports.log = log;
+module.exports = Logger;
